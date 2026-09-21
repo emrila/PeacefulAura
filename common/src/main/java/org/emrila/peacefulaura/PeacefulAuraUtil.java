b1a1542fdad6bf1c04de4ee15323164988a89e60
emrila <emmarichterlarsson@gmail.com>
2026-09-21 16:15:25 +0200

    refactor: self-contained and simplify effect checks used by mixin classes

--- common/src/main/java/org/emrila/peacefulaura/PeacefulAuraUtil.java
+++ common/src/main/java/org/emrila/peacefulaura/PeacefulAuraUtil.java
@@ -5,42 +5,20 @@ import net.minecraft.core.Holder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import org.emrila.peacefulaura.effect.ModEffectCondition;
 
 public final class PeacefulAuraUtil {
 
-    private static ModEffectCondition modEffectCondition = new ModEffectCondition(){};
-
-    public static void setModEffect(final Holder<MobEffect> effectHolder) {
-        PeacefulAuraUtil.modEffectCondition = new ModEffectCondition(){
-            @Override
-            public boolean isModEffect(MobEffectInstance effect) {
-                return effect.is(effectHolder);
-            }
-
-            @Override
-            public boolean hasModEffect(LivingEntity entity) {
-                return entity != null && entity.hasEffect(effectHolder);
-            }
-
-            @Override
-            public boolean hasEitherModEffect(LivingEntity initiator, LivingEntity target) {
-                return hasModEffect(initiator) || hasModEffect(target);
-            }
-        };
-    }
+    private static final Holder<MobEffect> PEACEFUL_AURA_EFFECT = ModUtil.getHolderForEffect();
 
     public static boolean hasAura(final LivingEntity entity) {
-        return modEffectCondition.hasModEffect(entity);
+        return entity != null && entity.hasEffect(PEACEFUL_AURA_EFFECT);
     }
 
     public static boolean isAura(final MobEffectInstance effect) {
-        return modEffectCondition.isModEffect(effect);
+        return effect.is(PEACEFUL_AURA_EFFECT);
     }
 
-    public static boolean preventTargeting(final LivingEntity attacker, final LivingEntity target){
-        return modEffectCondition.hasEitherModEffect(attacker, target);
+    public static boolean preventTargeting(final LivingEntity initiator, final LivingEntity target){
+       return hasAura(initiator) || hasAura(target);
     }
-
-
 }
